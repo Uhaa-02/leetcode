130080e9b49210cd4450b87e77fbbd0971ac9b0e
Uhaa-02 <23ag1a6908@gmail.com>
2026-08-04 22:28:07 +0530

    Time: 1 ms (97.45%), Space: 47.6 MB (22.92%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        int leftSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            // Check if left sum equals right sum
+            //left + nums[i] + right = tot --> right = tot - left - nums[i]
+            if (leftSum == totalSum - leftSum - nums[i]) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+}
